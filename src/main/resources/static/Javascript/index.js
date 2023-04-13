babe16b2ae698359f1012f891f4d706c131c31d2
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-04-13 16:24:48 +0200

    Ajout tri par ville + lien site internet des festivals dans la cartographie + lien retour vers page index sur page inscription

--- src/main/resources/static/Javascript/index.js
+++ src/main/resources/static/Javascript/index.js
@@ -14,10 +14,15 @@ fetch("http://localhost:8080/api/festivals")
         // Parcourez les données récupérées et créez un marqueur pour chaque festival
         data.forEach(festival => {
             var marker = L.marker([festival.latitude, festival.longitude]).addTo(map);
-            marker.bindPopup("<b>" + festival.nom + "</b><br>" + festival.ville + "<br>" + festival.lieu).openPopup();
+            var link = "<a href='" + festival.url + "'>" + festival.url + "</a>";
+            marker.bindPopup("<b>" + festival.nom + "</b><br>" + festival.ville + "<br>" + festival.lieu+ "<br>" + link).openPopup();
         });
     })
     .catch(error => console.error(error));
+
+
+//-----------------------------------------------------------------------------------------------------
+
 
 // Ajout de la possibilité de trier les festivals par date
 // Obtenir la colonne "Date de début"
@@ -49,6 +54,10 @@ dateDebutHeader.addEventListener("click", function() {
     rows.forEach(row => tbody.appendChild(row));
 });
 
+
+//--------------------------------------------------------------------------------------
+
+
 // Ajout de la possibilité de trier les festivals par nom
 // Obtenir la colonne "Nom du festival"
 const nomHeader = document.querySelector(".en-tete-tableau:nth-child(1)");
@@ -73,6 +82,40 @@ nomHeader.addEventListener("click", function() {
         nomHeader.dataset.sort = "desc";
     } else {
         nomHeader.dataset.sort = "asc";
+    }
+
+    // Mettre à jour le tableau avec les données triées
+    rows.forEach(row => tbody.appendChild(row));
+});
+
+
+//-----------------------------------------------------------------------------------------------------
+
+
+// Ajout de la possibilité de trier les festivals par ville
+// Obtenir la colonne "Ville du festival"
+const villeHeader = document.querySelector(".en-tete-tableau:nth-child(2)");
+
+// Ajouter un écouteur d'événements de clic à l'en-tête de la colonne "Ville du festival"
+villeHeader.addEventListener("click", function() {
+    const table = document.querySelector(".tableau");
+    const tbody = table.querySelector("tbody");
+    const rows = Array.from(tbody.querySelectorAll("tr"));
+
+    // Tri des données en fonction de la ville du festival
+    rows.sort(function(a, b) {
+        const aVille = a.querySelector(".cellule:nth-child(2)").innerText;
+        const bVille = b.querySelector(".cellule:nth-child(2)").innerText;
+
+        return aVille.localeCompare(bVille);
+    });
+
+    // Inverser l'ordre si l'utilisateur a déjà trié dans l'ordre croissant
+    if (villeHeader.dataset.sort === "asc") {
+        rows.reverse();
+        villeHeader.dataset.sort = "desc";
+    } else {
+        villeHeader.dataset.sort = "asc";
     }
 
     // Mettre à jour le tableau avec les données triées

ab5d6a39544a8959e105f9926fa757b6da912b8e
Pierre <121941634+PierreVlaeminck@users.noreply.github.com>
2023-04-13 09:33:23 +0200

    Ajout des commentaires JavaDoc au code Java.

--- src/main/java/fr/simplon/festivals/controller/FestivalsController.java
+++ src/main/java/fr/simplon/festivals/controller/FestivalsController.java
@@ -26,14 +26,14 @@ public class FestivalsController {
      * @return String representing the name of the view to be rendered for displaying the list of Festivals.
      */
     @GetMapping("/")
-    public String displayAllFestivals(Model model){
+    public String displayAllFestivals(Model model) {
         List<Festival> festivals = festivalsDao.getAllFestivals();
         model.addAttribute("festivals", festivals);
         return "index";
     }
 
     @GetMapping("/inscription")
-    public String displayForms(Model model){
+    public String displayForms(Model model) {
         model.addAttribute("festival", new Festival());
         return ("inscription");
     }
@@ -50,9 +50,15 @@ public class FestivalsController {
         return ("redirect:/");
     }
 
-
+    /**
+     * Displays the form for editing an existing Festival.
+     *
+     * @param id Long representing the ID of the Festival to be edited.
+     * @param model Model object to be used for storing and accessing data to be displayed on the view.
+     * @return String representing the name of the view to be rendered for displaying the form.
+     */
     @GetMapping("/modification")
-    public String displayEditForm(@RequestParam("id") Long id, Model model){
+    public String displayEditForm(@RequestParam("id") Long id, Model model) {
         Optional<Festival> optionalFestival = festivalsDao.findById(id);
         if (optionalFestival.isPresent()) {
             Festival festival = optionalFestival.get();
@@ -64,7 +70,12 @@ public class FestivalsController {
         }
     }
 
-
+    /**
+     * Updates an existing Festival in the database.
+     *
+     * @param festival Festival object representing the Festival to be updated.
+     * @return String representing the name of the view to be rendered after updating the Festival.
+     */
     @PostMapping("/modification")
     public String editFestival(@ModelAttribute("festival") Festival festival) {
 
@@ -96,7 +107,11 @@ public class FestivalsController {
         }
     }
 
-
+    /**
+     * Retrieves all Festivals from the database and returns them in JSON format for use in API requests.
+     *
+     * @return A List of Festival objects in JSON format.
+     */
     @GetMapping("/api/festivals")
     @ResponseBody
     public List<Festival> getAllFestivalsAsJson() {
--- src/main/java/fr/simplon/festivals/dao/impl/FestivalsDaoImpl.java
+++ src/main/java/fr/simplon/festivals/dao/impl/FestivalsDaoImpl.java
@@ -8,27 +8,42 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Implementation of FestivalsDao interface for accessing and modifying Festival data in the database.
+ */
 @Repository
 public class FestivalsDaoImpl implements FestivalsDao {
     @Autowired
     private FestivalsRepository festivalsRepository;
 
 
-    // Méthode pour enregistrer un nouveau festival dans la base
+    /**
+     * Saves a new Festival object to the database.
+     *
+     * @param festival Festival object to be saved to the database.
+     */
     @Override
-    public void saveFestivals(Festival festival){
+    public void saveFestivals(Festival festival) {
         festivalsRepository.save(festival);
     }
 
 
-    // Récupère tous les festivals present dans la base de données.
+    /**
+     * Retrieves a list of all Festivals from the database.
+     *
+     * @return List of all Festivals present in the database.
+     */
     @Override
-    public List<Festival> getAllFestivals(){
+    public List<Festival> getAllFestivals() {
         return festivalsRepository.findAll();
     }
 
-
-    // Récupération du festival sélectionné par son ID en cliquant sur le bouton "Editer" .
+    /**
+     * Retrieves a Festival from the database based on its ID.
+     *
+     * @param id ID of the Festival to be retrieved.
+     * @return Optional of Festival object with the given ID.
+     */
     @Override
     public Optional<Festival> findById(Long id) {
         return festivalsRepository.findById(id);
